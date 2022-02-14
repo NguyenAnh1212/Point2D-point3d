42cfe512bf4a26ae57e6a2e09716b86307b4b368
unknown <anhnt.tb12@gmail.com>
2022-02-14 23:06:40 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Point2D p2d = new Point2D(1,2);
+        Point3D p3d = new Point3D(1,2,5);
+        System.out.println(p3d.toString());
+    }
+}
